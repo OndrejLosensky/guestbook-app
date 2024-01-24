c2bd0c46de5f77346a977cc9ec5d351b24c9fb47
Ondrej Losensky <ondra.losi@gmail.com>
2024-01-24 16:58:19 +0100

    styling for the page

--- generate_data/InsertLoop.php
+++ generate_data/InsertLoop.php
@@ -4,9 +4,93 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Proccess the generated data!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            opacity: 1;
+            padding: 20px;
+            margin: 0;
+            padding-left: 20px;
+            position: relative;
+        }
+
+        body::before {
+            content: "";
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: radial-gradient(rgba(0, 0, 0, 0.15) 1px, transparent 2px);
+            background-size: 35px 35px;
+            opacity: 0.65;
+            pointer-events: none;
+            z-index: -1; 
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 10px;
+            border: 1px solid #ddd;
+            text-align: left;
+        }
+
+        th {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        tr:hover {
+            background-color: #ddd;
+        }
+
+        .button-link {
+            display: inline-flex;
+            align-items: center;
+            text-decoration: none;
+            background-color: #3498db;
+            color: #fff;
+            padding: 10px 15px; 
+            border-radius: 5px;
+        }
+
+        .button-icon {
+            width: 20px;
+            height: 20px; 
+            margin-right: 10px; 
+        }
+
+    </style>
+
+    </style>
 </head>
 <body>
     <h1>Your data was generated</h1>
+
+    <a href="/GuestBook_GITHUB/index.html" class="button-link">
+            <img src="/GuestBook_GITHUB/assets/arrowIcon.png" alt="Icon" class="button-icon">
+            Back to main page
+    </a>
+
     <p>
         <?php
             $mydb = new mysqli("localhost", "root");
@@ -54,8 +138,6 @@
 
         ?>
     </p>
-
-    <a href="/GuestBook_GITHUB/index.html"> Back to main page</a>
 
 </body>
 </html>
